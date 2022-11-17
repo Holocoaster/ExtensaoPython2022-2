0b495a9b880edeaf188976c3283c9f77816818bb
Lemongate <116230630+Holocoaster@users.noreply.github.com>
2022-11-17 01:02:33 +0000

    New BD files

--- main.py
+++ main.py
@@ -10,9 +10,22 @@ numeric_type = input("Type 1 for HERO and 2 for VILLAIN: ")
 
 #Checks the max value within the list
 
-sql = "SELECT MAX(pessoa_id) FROM pessoas"
+sql = "SELECT MAX(pessoa_id)+1 FROM pessoas"
 cursor.execute(sql)
 cursor.fetchone()
 
+pessoa_id = cursor.fetchone()[0]
 
+#Convertion from number keys in line 9
 
+if numeric_type == "1":
+  type = "HERO"
+
+else:
+  type = "VILLAIN"
+
+sql =f"INSERT INTO pessoas (pessoa_id), nome, nome_civil, tipo) VALUES ({pessoa_id}, '{nome}', '{nome_civil', '{tipo}')"
+
+cursor.execute(sql)
+connection.commit()
+connection.close()
